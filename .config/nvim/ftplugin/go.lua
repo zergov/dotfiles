5bc5d4589e12b42c93217a84072cfa574274cefc
Jonathan Lalande <lalandej.gg@gmail.com>
2025-06-04 21:30:29 -0400

    add ftplugin/go.lua

--- .config/nvim/ftplugin/go.lua
+++ .config/nvim/ftplugin/go.lua
@@ -1,3 +1,12 @@
+--------------------------------------------------------------
+--	  Defaults Tabs and indent
+--------------------------------------------------------------
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.autoindent = true
+vim.o.wrap = false
+
 vim.lsp.start({
   name = 'gopls',
   cmd = {'gopls'},
